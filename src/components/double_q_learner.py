Fixed small error in using incorrect target matrix to calculate optimal step from next state. Added function to output 100pt average statistics to window and log.

src/components/double_q_learner.py
@@ -79,7 +79,7 @@ class DoubleQLearner():
 
             # Get best actions 
             a_1 = actions
-            a_2 = torch.argmax(Q_2_preds,dim=1)
+            a_2 = torch.argmax(torch.tensor(Q_1(s_2_mat)).clone(),dim=1)
 
             # Extract other useful info from batch data
             # rews = torch.unsqueeze(rewards,1)