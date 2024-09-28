Added weight initialization and also fixed bug when using torch.gather. Was indexing on wrong axis.

src/components/ann_model.py
@@ -25,7 +25,7 @@ Relu as activation function, and Adam optimizer with definable learning rate. NO
 '''
 class DoubleQLearnerANN(nn.Module):
     def __init__(self,num_layers,neurons,num_inputs=8,num_outputs=4,loss=nn.MSELoss(),learn_rate=0.0001,
-                 learn_rate_decay=1.0,learn_rate_min=1e-6,steps_to_update=100):
+                 learn_rate_decay=1.0,learn_rate_min=1e-6,steps_to_update=100,max_steps=10000):
         super().__init__()
         self.num_inputs = num_inputs
         self.num_layers = num_layers
@@ -52,12 +52,14 @@ class DoubleQLearnerANN(nn.Module):
 
             # Create sequential model
             self.ANN_relu = nn.Sequential(*layers).to(device)
+            self.apply(self.initialize_weights)
                 
         except Exception as e:
             raise CustomException(e,sys)
         
         self.optimizer = torch.optim.Adam(self.ANN_relu.parameters(),lr=learn_rate)
-        self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[steps_to_update], 
+        self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, 
+                                                              milestones=torch.arange(steps_to_update,max_steps,steps_to_update),
                                                               gamma=learn_rate_decay)
         logging.info(f"Neural network initialized.")
         
@@ -72,6 +74,12 @@ class DoubleQLearnerANN(nn.Module):
             f'loss function = {self.loss_fcn}{n1}'
             f'learn_rate = {self.learn_rate}{n1}'
             )
+    
+    def initialize_weights(self,module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.xavier_uniform_(module.weight)
+            if module.bias is not None:
+                module.bias.data.zero_()
     
     def forward(self,x):
         out = self.ANN_relu(DataLoader(x))

src/components/double_q_learner.py
@@ -15,11 +15,11 @@ This freezing of target nets allows for more stable training.
 '''
 class DoubleQLearner():
     def __init__(self,num_layers,neurons,num_inputs=8,loss=nn.MSELoss(),num_actions=4,buf_size=50000,batch_size=32,
-                 alpha=0.0001,alpha_decay=1.0,alpha_min=1e-6,gamma=0.99,eps=1.0,steps_to_update=10000):
-        self.Q_a_obj = DoubleQLearnerANN(num_layers,neurons,num_inputs,num_actions,loss,alpha,alpha_decay,alpha_min,steps_to_update/2)
-        self.Q_b_obj = DoubleQLearnerANN(num_layers,neurons,num_inputs,num_actions,loss,alpha,alpha_decay,alpha_min,steps_to_update/2)
-        self.Q_a_obj_target = DoubleQLearnerANN(num_layers,neurons,num_inputs,num_actions,loss,alpha,alpha_decay,alpha_min,0)
-        self.Q_b_obj_target = DoubleQLearnerANN(num_layers,neurons,num_inputs,num_actions,loss,alpha,alpha_decay,alpha_min,0)
+                 alpha=0.0001,alpha_decay=1.0,alpha_min=1e-6,gamma=0.99,eps=1.0):
+        self.Q_a_obj = DoubleQLearnerANN(num_layers,neurons,num_inputs,num_actions,loss,alpha,alpha_decay,alpha_min)
+        self.Q_b_obj = DoubleQLearnerANN(num_layers,neurons,num_inputs,num_actions,loss,alpha,alpha_decay,alpha_min)
+        self.Q_a_obj_target = DoubleQLearnerANN(num_layers,neurons,num_inputs,num_actions,loss,alpha,alpha_decay,alpha_min)
+        self.Q_b_obj_target = DoubleQLearnerANN(num_layers,neurons,num_inputs,num_actions,loss,alpha,alpha_decay,alpha_min)
         self.Q_a = self.Q_a_obj.ANN_relu
         self.Q_b = self.Q_b_obj.ANN_relu
         self.Q_a_target = self.Q_a_obj_target.ANN_relu
@@ -89,7 +89,7 @@ class DoubleQLearner():
             # NOTE: sample in batch = x_1,x_2,a_1,reward,buf_done (rows)
             targets = Q_1_preds # initialize equal to outputs 
         
-            targets[torch.arange(self.batch_size).long(),a_1] = (rews + self.gamma * Q_2_preds[:].gather(0,a_2.view(self.batch_size,1)) * (1 - done.int())).view(self.batch_size)
+            targets[torch.arange(self.batch_size).long(),a_1] = (rews + self.gamma * Q_2_preds[:].gather(1,a_2.view(self.batch_size,1)) * (1 - done.int())).view(self.batch_size)
 
             return targets,a_1
         