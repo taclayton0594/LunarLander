b8bbb188f03ee0e2d77bc070af5d418f66df7ae4
taclayton0594 <taylor2@alumni.stanford.edu>
2024-11-10 08:33:05 -0600

    Removed Flask app from code. Will create a Flask app for my LLM project. Added images to Readme.

--- application.py
+++ application.py
@@ -1,21 +1,5 @@
-from flask import Flask,request,render_template
 from src.components.model_trainer import RLModelTrainer
 
-application = Flask(__name__)
-
-app = application
-
-## Route for the home page
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-#@app.route('/LunarRover',methods=['GET','POST'])
-#def run_lunar_rover():
-
-
 if __name__=="__main__":
-    #app.run(host='0.0.0.0', port=8080)      
-
     LunarLanderMdl = RLModelTrainer()
     LunarLanderMdl.start_RL_training()
